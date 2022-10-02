7e3d46dc2b320c1ffc945d5bf8ed1377513f00bf
Eldar <eldarweb@mail.ru>
2022-10-02 17:06:21 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
 
-  get 'parsings' => "parsings#index"
-
-  post 'parsings/launch'   
+  root 'parsings#index'   
 
   get 'bitcoin_tema' => 'parsings#bitcoin_tema'
 
