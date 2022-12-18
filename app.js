4dae81385343760764503d16a600bc9d0ed0b071
Eldar <eldarweb@mail.ru>
2022-12-19 02:05:04 +0300

    fix

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ app.use(express.static(path.join(__dirname)))
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
+app.listen(3333, () => {
+    console.log('Application listening on port 3333!');
+});
