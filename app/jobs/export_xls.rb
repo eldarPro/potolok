b592ffc2fe6db66c6c95e041f0271e1cd46e925b
Eldar <eldarweb@mail.ru>
2022-12-19 01:13:50 +0300

    fix

--- app/jobs/export_xls.rb
+++
@@ -1,8 +0,0 @@
-class ExportXls 
-
-    include Sidekiq::Worker 
-        
-    def perform cat_temp, list_barcodes, list_emails
-        ExportXls.new(cat_temp, list_barcodes, list_emails).call
-    end 
-  end
--- config/initializers/mime_types.rb
+++
@@ -1,3 +0,0 @@
-Mime::Type.register "application/xls", :xls
-Mime::Type.register "application/pdf", :pdf
-Mime::Type.register "application/xlsx", :xlsx
