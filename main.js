eb60bd9a6ba440b0a794990d3d551b56ffb80a9e
Eldar <eldarweb@mail.ru>
2023-01-30 18:15:29 +0300

    fix

--- main.js
+++ main.js
@@ -149,5 +149,5 @@ const priceRoom = (item) => {
   return res / 100
 }
 
-//var host = 'http://127.0.0.1:3000'
-var host = 'https://potolokapi-production.up.railway.app'
+var host = 'http://127.0.0.1:3000'
+//var host = 'https://potolokapi-production.up.railway.app'
