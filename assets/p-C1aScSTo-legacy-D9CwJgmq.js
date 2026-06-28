338012ff2fd692f51ba529dc50cdcf32de7d07f0
eldarPro <eldarPro@users.noreply.github.com>
2026-06-28 22:53:07 +0000

    deploy: 05dccf2340874b4ac4b90c28f2fdf930e11e36ea

--- assets/p-C1aScSTo-legacy-D9CwJgmq.js
+++ assets/p-C1aScSTo-legacy-D9CwJgmq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-kbllzPlh.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
+System.register(["./index-legacy-4mtpQoYE.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
